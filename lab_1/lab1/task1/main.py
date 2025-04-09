0332a271842129696007ae100a3df994cc4ff6f6
Polina <1vistu1@gmail.ru>
2025-04-09 10:46:53 +0400

    добавлены докстринги

--- lab_1/lab1/task1/main.py
+++ lab_1/lab1/task1/main.py
@@ -5,11 +5,23 @@ from constants import *
 
 
 def read_file(filename):
+    """
+    Читает содержимое файла и возвращает его в виде строки.
+
+    :param filename: Путь к файлу, который нужно прочитать.
+    :return: Содержимое файла.
+    """
     with open(filename, "r", encoding=FILE_ENCODING) as file:
         return file.read()
 
-# Функция для шифрования в обратном порядке алфавита
+
 def reverse_alphabet_cipher(text):
+    """
+    Шифрует текст, заменяя каждую букву на противоположную ей в алфавите.
+
+    :param text: Исходный текст для шифрования.
+    :return: Зашифрованный текст.
+    """
     encrypted_text = ''
     for char in text:
         if char.isupper():
@@ -21,32 +33,50 @@ def reverse_alphabet_cipher(text):
     return encrypted_text
 
 
-def  save_encrypted_text(rev_encrypted_text):
+def save_encrypted_text(rev_encrypted_text):
+    """
+    Сохраняет зашифрованный текст в файл.
+
+    :param rev_encrypted_text: Зашифрованный текст.
+    """
     with open(OUTPUT_FILE_PATH, "w", encoding=FILE_ENCODING) as f:
         f.write(rev_encrypted_text)
 
 
 def generate_decipher_key():
+    """
+    Генерирует ключ для расшифровки зашифрованного текста.
+
+    :return: Словарь, содержащий пары символ-значение для расшифровки.
+    """
     return {char: key for key, char in DECIPHER_DICT_UPPER.items()}
 
 
 def save_decipher_key(decipher_key):
-    with open("decipher_key.json", "w", encoding=FILE_ENCODING) as f:
+    """
+    Сохраняет ключ для расшифровки в файл.
+
+    :param decipher_key: Ключ для расшифровки.
+    """
+    with open(KEY_FILE_PATH, "w", encoding=FILE_ENCODING) as f:
         json.dump(decipher_key, f, indent=4, ensure_ascii=False)
 
 
 def main():
+    """
+    Основная функция программы. Читает текст из файла, шифрует его, сохраняет зашифрованный текст и ключ для расшифровки.
+    """
     try:
         # Читаем исходный текст из файла
         original_text = read_file(INPUT_FILE_PATH)
 
-        # Шифрование текста
+        # Шифруем текст
         rev_encrypted_text = reverse_alphabet_cipher(original_text)
 
-        # Сохранение результатов
+        # Сохраняем зашифрованный текст
         save_encrypted_text(rev_encrypted_text)
 
-        # Сохранение ключа расшифровки в отдельный файл
+        # Генерируем и сохраняем ключ для расшифровки
         decipher_key = generate_decipher_key()
         save_decipher_key(decipher_key)
 
