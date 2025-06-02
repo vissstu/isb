83f610f0ecad319ceb2b79f97ddc00d1738b85c4
Polina <1vistu1@gmail.ru>
2025-06-02 04:29:34 +0400

    удаление 2 лабы

--- lab_2/lab2/const.py
+++
@@ -1,6 +0,0 @@
-# Файлы с бинарными последовательностями
-BIN_SEQ_CPP = "cpp_binary_sequence.txt"
-BIN_SEQ_JAVA = "java_binary_sequence.txt"
-
-# Выходной файл с результатами тестов
-RESULTS_FILE = "test_results.txt"
